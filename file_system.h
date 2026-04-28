7b3d0cb0a8a529c3b37001ab042378958c404220
LnY5 <u3659001@connect.hku.hk>
2026-04-28 20:55:45 +0800

    Update file_system.h

--- file_system.h
+++ file_system.h
@@ -3,14 +3,11 @@
 #include "game_shared.h"
 #include <string>
 
-// What it does: Writes player stats and dynamic array contents to a text file
-// Inputs: Constant reference to Player struct, string filename
-// Outputs: Creates or overwrites a text file
 void saveGame(const Player& p, std::string filename);
-
-// What it does: Reads data from text file, deletes old backpack, and creates a new one
-// Inputs: Reference to Player struct, string filename
-// Outputs: Returns true if load was successful, false if file missing or corrupted
 bool loadGame(Player& p, std::string filename);
+
+void autoSave(const Player& p);
+
+void showDayStartSummary(const Player& p);
 
 #endif
